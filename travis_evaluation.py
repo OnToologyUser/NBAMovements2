0ca27766cc3626f242ee3957f465a44b00fe0b84
albaizq <albaizq13@gmail.com>
2016-06-04 00:41:24 +0200

    Update travis_evaluation.py

--- travis_evaluation.py
+++ travis_evaluation.py
@@ -122,6 +122,8 @@ def read_query(req_file):
 	if type(row)  != types.BooleanType:
 	    for element in row: 
 	    		row_element.append(str(element))
+	    		print row
+	    		print row[0]
 	    		if 'URI' in row:
 	    			row_element_type.append('uri')
 	    		elif 'Literal' in row:
